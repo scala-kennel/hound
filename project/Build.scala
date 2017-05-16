19c8409904e107675a51eff12121e3623c4c0e7d
pocketberserker <pocketberserker@gmail.com>
2017-05-17 08:26:20 +0900

    dog 0.8.0

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 object Build {
 
   private object Version {
-    val dog = "0.7.0"
+    val dog = "0.8.0"
   }
 
   private def gitHash: String = scala.util.Try(
